4cd1fa686d8ca1fdd833d245ae921ffb648811df
Tyler <tyler.l.filbert@gmail.com>
2023-10-31 17:13:38 -0400

    Correct implementation for simple query

--- RecipeLoading/recipe_queries.py
+++ RecipeLoading/recipe_queries.py
@@ -1,21 +1,27 @@
 import os
 import csv
+import botocore
 import boto3
 from boto3.dynamodb.conditions import Attr
+from boto3.dynamodb.conditions import Key
 
 
-dynamodb = boto3.client('dynamodb', region_name='us-east-2')
+dynamodb = boto3.resource('dynamodb')
 
 table_compound_ingredients = 'compound_ingredients'
 table_ingredients = 'ingredients'
 table_recipes_by_ingredients = 'recipes_by_ingredients'
 table_recipe_details = 'recipe_details'
 
-response = dynamodb.scan(
-    TableName=table_ingredients,
-    FilterExpression=Attr('ingredient_name').eq('pepper')
-)
+table_ingredients = 'ingredients'
+ingredient_name_to_query = 'Buckwheat'  # Replace with the ingredient name you want to query
 
-items = response['Items']
-for item in items:
-    print("Item:", item)
+table = dynamodb.Table(table_ingredients)
+
+fe = Attr('ingredient_name').eq("Buckwheat");
+
+response = table.scan(
+        FilterExpression=fe        
+    )
+for item in response['Items']:
+  print(item)
