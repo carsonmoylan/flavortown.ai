b52edc9c8aa45a0af5729f5c3df55a9f7ede8b32
ColinCarver10 <ccarver410@gmail.com>
2023-11-06 20:13:17 -0500

    Integrated with Bart push. User can upload image and classification will be returned to console.

--- src/personal/forms.py
+++ src/personal/forms.py
@@ -2,8 +2,15 @@ from django.forms import ModelForm
 from django import forms
 from .models import Food
 
-class UploadForm(ModelForm):
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class' : 'ui active button'}))
+from django import forms
+
+class UploadForm(forms.ModelForm):
+    image = forms.ImageField(
+        widget=forms.FileInput(attrs={'class': 'ui active button', 'style': 'display: none;', 'id': 'imageUpload'}),
+        label=''
+    )
+
     class Meta:
         model = Food
         fields = ['image']
+
