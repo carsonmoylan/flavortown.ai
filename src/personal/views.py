edb4ff9123b8bf86ba174b32bfd00895f23e6419
ColinCarver10 <ccarver410@gmail.com>
2023-11-16 16:23:19 -0500

    Added button to go back and clean up code.

--- src/personal/views.py
+++ src/personal/views.py
@@ -6,14 +6,11 @@ import json
 import sys
 from django.conf import settings
 from pathlib import Path
-from . import getCommonRecipes
+#from . import getCommonRecipes
 
 # Create your views here.
 def home_screen_view(request):
     if request.method == 'POST':
-        if 'ingredients' in request.POST:
-            ingredients = request.POST.getlist('ingredients')
-            #rec_recipes = getCommonRecipes.getRecRecipes(ingredients)
         imageForm = ImageForm(request.POST, request.FILES)
         imageName = request.FILES[u'image'].name
 
@@ -25,7 +22,6 @@ def home_screen_view(request):
             return redirect('display_image_with_classes', imageClasses=array_param)
     else:
         imageForm = ImageForm()
-        ingredients_form = IngredientForm()
 
     
     return render(request, "personal/home.html", {'imageForm' : ImageForm})
@@ -42,6 +38,6 @@ def display_recipes(request):
         print(request.POST)
         if 'ingredients[]' in request.POST:
             ingredients = request.POST.getlist('ingredients[]')
-            rec_recipes = getCommonRecipes.getRecRecipes(ingredients)
+            #rec_recipes = getCommonRecipes.getRecRecipes(ingredients)
 
     return render(request, 'personal/recipesView.html', {})
