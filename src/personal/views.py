d4e63a82e01ee92cfe90ffaa9278ce488e14759a
ColinCarver10 <ccarver410@gmail.com>
2023-11-29 12:16:01 -0500

    Update views.py

--- src/personal/views.py
+++ src/personal/views.py
@@ -19,6 +19,7 @@ def home_screen_view(request):
         if imageForm.is_valid():
             imageForm.save()
             imageName =  Food.objects.last().image.name
+            print(imageName)
             imageInfo = getImageInfo(imageName) # Returns array of class names.
             array_param = ','.join(imageInfo)
             request.session['image_classes'] = array_param
